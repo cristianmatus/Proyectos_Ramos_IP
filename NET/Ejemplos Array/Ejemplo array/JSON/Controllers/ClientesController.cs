8a7d762fe950da6b7fb86108c19c050232899fdc
Crmatus19 <94331686+cristianmatus@users.noreply.github.com>
2022-05-09 13:22:51 -0400

    Add files via upload

---
+++ NET/Ejemplos Array/Ejemplo array/JSON/Controllers/ClientesController.cs
@@ -0,0 +1,25 @@
+﻿using JSON_Example.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JSON_Example.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        //public ActionResult Clientes()
+        //{
+        //    return View();
+        //}
+
+        //generar un JSON  a partir de mi objeto  / modelo
+        public ActionResult Clientes()
+        {
+            var cli = new Clientes() { Nombre = "Carlos", Apellido = "Guzman", Direccion = "Macul", Edad = "30" };
+            return Json(cli, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
