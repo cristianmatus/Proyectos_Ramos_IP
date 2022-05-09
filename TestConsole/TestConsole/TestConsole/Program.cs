00580a47131b2e2c1cc2df6d5fdaac724f888b28
Crmatus19 <94331686+cristianmatus@users.noreply.github.com>
2022-05-08 22:28:39 -0400

    Add files via upload

---
+++ TestConsole/TestConsole/TestConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese valor");
+            Console.WriteLine("-------------");
+
+            int caseSwitch = 0;
+
+            switch(caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Este es el primer caso");
+                    break;
+                case 2:
+                    Console.WriteLine("Este es el segundo caso");
+                    break;
+                default:
+                    Console.WriteLine("Este es el tercer caso");
+                    break;
+            }
+        }
+    }
+}
