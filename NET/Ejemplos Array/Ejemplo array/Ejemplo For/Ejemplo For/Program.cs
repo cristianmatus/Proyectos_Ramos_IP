121d855478b0df9ab78225a805295f66b7441e19
Crmatus19 <94331686+cristianmatus@users.noreply.github.com>
2022-05-09 13:21:08 -0400

    Add files via upload

---
+++ NET/Ejemplos Array/Ejemplo array/Ejemplo For/Ejemplo For/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejemplo_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese primer dato: ");
+            int dato1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese segundo dato: ");
+            int dato2 = Convert.ToInt32(Console.ReadLine());
+            
+            if (dato1 < dato2)
+                for (int i = dato1; i <= dato2; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            if (dato1 > dato2)
+                for (int i = dato1; i >= dato2; i--)
+                {
+                    Console.WriteLine(i);
+                }
+            else
+                Console.WriteLine("son iguales");
+
+            Console.ReadKey();
+        }
+    }
+}
