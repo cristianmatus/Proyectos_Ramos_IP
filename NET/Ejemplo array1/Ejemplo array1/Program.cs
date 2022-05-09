b1fcb2546f0318100bacd7d5d20fe3198aeb5d95
Crmatus19 <94331686+cristianmatus@users.noreply.github.com>
2022-05-09 13:19:59 -0400

    Add files via upload

---
+++ NET/Ejemplo array1/Ejemplo array1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejemplo_array1
+{
+    class Program
+    {              
+        static void Main(string[] args)
+        {
+
+
+            string[] Ejemplo_array1 = new string[6];
+            Ejemplo_array1[0] = "Hola";
+            Ejemplo_array1[1] = "Bonita";
+            Ejemplo_array1[2] = "Espero";
+            Ejemplo_array1[3] = "Que";
+            Ejemplo_array1[4] = "Te";
+            Ejemplo_array1[5] = "Mejores";
+            int i = 0;
+            
+            //for (int i = 0; i < Ejemplo_array1.Length; i++)
+            //{
+             //   Console.WriteLine(" " + Ejemplo_array1[i]);
+            //}
+
+            foreach (var item in Ejemplo_array1)
+            {
+                Console.WriteLine(" " + Ejemplo_array1[i]);
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NET/Ejemplo array2/Ejemplo array2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejemplo_array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Ejemplo_array2 = new int[5];
+
+            Console.WriteLine("Ingrese valor");
+            Ejemplo_array2[0] = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < Ejemplo_array2.Length; i++)
+            {
+                Console.WriteLine(Ejemplo_array2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
