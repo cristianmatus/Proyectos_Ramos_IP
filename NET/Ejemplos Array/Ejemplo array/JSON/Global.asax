34f5ee0b49da2d9442c82db66c0a9b81b220a5ce
Crmatus19 <94331686+cristianmatus@users.noreply.github.com>
2022-05-09 13:25:10 -0400

    Add files via upload

---
+++ NET/Ejemplos Array/Ejemplo array/JSON/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JSON_Example.MvcApplication" Language="C#" %>
---
+++ NET/Ejemplos Array/Ejemplo array/JSON/Models/Clientes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JSON_Example.Models
+{
+    public class Clientes
+    {
+        public string nombre;
+        public string apellido;
+        public string direccion;
+        public int edad;
+
+        public Clientes()
+        {
+            Nombre = "Cristian";
+            Apellido = "Matus";
+            Direccion = "Macul";
+            Edad = 29;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public int Edad { get => edad; set => edad = value; }
+    }
+}
